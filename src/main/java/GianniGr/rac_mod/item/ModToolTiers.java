b8d4e29d39cb4316a97c40aab0e5a7e2435b7f2f
Gianni <gianni110402@gmail.com>
2025-04-08 22:46:36 +0200

    adding armor and tools WIP

---
+++ src/main/java/GianniGr/rac_mod/item/ModToolTiers.java
@@ -0,0 +1,19 @@
+package GianniGr.rac_mod.item;
+
+import GianniGr.rac_mod.Rac_mod;
+import GianniGr.rac_mod.util.ModTags;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+import java.util.List;
+
+public class ModToolTiers {
+    public static final Tier SAPPHIRE = TierSortingRegistry.registerTier(
+            new ForgeTier(5, 1500, 5f, 4f, 25, ModTags.Blocks.NEEDS_SAPPHIRE_TOOL, () -> Ingredient.of(ModItems.SAPPHIRE.get())),
+            new ResourceLocation(Rac_mod.MOD_ID, "sapphire"), List.of(Tiers.NETHERITE), List.of());
+
+}
