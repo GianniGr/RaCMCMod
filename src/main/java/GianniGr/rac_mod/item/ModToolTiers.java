6442f222f2712638ca996e8f39749ad57ce603f9
Gianni <gianni110402@gmail.com>
2025-04-09 13:50:04 +0200

    small refactor

--- src/main/java/GianniGr/rac_mod/item/ModToolTiers.java
+++ src/main/java/GianniGr/rac_mod/item/ModToolTiers.java
@@ -13,7 +13,8 @@ import java.util.List;
 
 public class ModToolTiers {
     public static final Tier SAPPHIRE = TierSortingRegistry.registerTier(
-            new ForgeTier(5, 1500, 5f, 4f, 25, ModTags.Blocks.NEEDS_SAPPHIRE_TOOL, () -> Ingredient.of(ModItems.SAPPHIRE.get())),
+            new ForgeTier(5, 1500, 5f, 4f, 25,
+                    ModTags.Blocks.NEEDS_SAPPHIRE_TOOL, () -> Ingredient.of(ModItems.SAPPHIRE.get())),
             new ResourceLocation(Rac_mod.MOD_ID, "sapphire"), List.of(Tiers.NETHERITE), List.of());
 
 }
